db0c8d6c9320f55c1e782f68df9413df0d3d66b4
Joshua Scholl <31521123+JschollDev@users.noreply.github.com>
2026-07-25 17:05:32 -0500

    PUSHING HARDER

--- truck-town-demo/vehicles/vehicle.gd
+++ truck-town-demo/vehicles/vehicle.gd
@@ -3,7 +3,6 @@ extends VehicleBody3D
 const STEER_SPEED = 1.5
 const STEER_LIMIT = 0.4
 const BRAKE_STRENGTH = 2.0
-@export var PLAYER_ID = 0
 
 @export var controller_id: int = 0
 @export var engine_force_value := 40.0
