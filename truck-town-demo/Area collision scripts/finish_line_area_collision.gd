119ca1e6b9afafd9df0f921e66e9d59aecce924d
SummerPassionfruit <github.com.rudder037@passmail.com>
2026-07-25 17:48:39 -0500

    Put area scripts in a folder

---
+++ truck-town-demo/Area collision scripts/finish_line_area_collision.gd
@@ -0,0 +1,15 @@
+extends CollisionShape3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_finish_line_area_entered(area: Area3D) -> void:
+	pass 
+	# Make it go to then end scene. Display winner of the one who crossed the line
