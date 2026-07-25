cd87b3be71844682ed24a0f91f3ccf7fa708687b
Joshua Scholl <31521123+JschollDev@users.noreply.github.com>
2026-07-25 18:58:09 -0500

    Updates

--- truck-town-demo/fade_away.gd
+++ truck-town-demo/fade_away.gd
@@ -1,5 +1,7 @@
 extends Panel
 
+@export var fade_time:float = 3.0
+
 func _ready() -> void:
-	await get_tree().create_timer(2.0).timeout
+	await get_tree().create_timer(fade_time).timeout
 	create_tween().tween_property(self, "modulate:a", 0.0, 2.0)
