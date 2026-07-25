4cb4b9cce0062413f1d07ebe9eb82c2d879bb05b
SummerPassionfruit <github.com.rudder037@passmail.com>
2026-07-25 15:20:07 -0500

    Created buffs ancient artifact, purple mushroom, and claypot

---
+++ truck-town-demo/ancient_artifact_area.gd
@@ -0,0 +1,14 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_ancient_artifact_area_entered(area: Area3D) -> void:
+	pass # Replace with function body.
