6f78390d55ac5193d8830fd7b1d1a3522b80f527
Joshua Scholl <31521123+JschollDev@users.noreply.github.com>
2026-07-25 22:19:28 -0500

    FUCKERY

--- truck-town-demo/winnder_screen.gd
+++ truck-town-demo/winnder_screen.gd
@@ -1,9 +1,6 @@
 extends Panel
 
-
-@export var next_scene: PackedScene
 @export var winner_label: Label
-
 
 func _ready() -> void:
 	hide()
@@ -13,16 +10,9 @@ func _ready() -> void:
 func _on_state_changed(new_state: GameManager.State) -> void:
 	if new_state != GameManager.State.WIN:
 		return
-
 	show_winner(GameManager.winner)
 
 
 func show_winner(x: int) -> void:
 	winner_label.text = "Player #%s Wins!" % x
 	show()
-
-	await get_tree().create_timer(3.0).timeout
-
-	GameManager.current_state = GameManager.State.SELECTION_SCREEN
-	GameManager.winner = 0
-	get_tree().change_scene_to_packed(next_scene)
